0e8f250bd209434d13842705143e2574b8d50690
Gabriel Barina <barinagabriel2007@yahoo.com>
2014-07-31 17:45:21 +0300

    continued work on depot, added portal for customers;

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -33,7 +33,7 @@ class ProductTest < ActiveSupport::TestCase
 	end
 
 	test "image url" do
-		ok = %w{ fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/zfred.gif }
+		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/zfred.gif }
 		bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
 		ok.each do |name|
